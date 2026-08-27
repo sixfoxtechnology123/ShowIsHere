2c7f099f3b9b48152df4b04ae81b6eb0363304b7
Bimal maity <bimalmaity8440@gmail.com>
2026-08-27 22:19:48 +0530

    next

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://localhost:5005/api';
+const API_BASE_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:5005/api' 
+  : '/api';
 
 const request = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
