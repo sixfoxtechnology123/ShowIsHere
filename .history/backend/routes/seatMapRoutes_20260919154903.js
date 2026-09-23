dadbc952897a7688a3181e15cd46f90c70a93b61
Bimal maity <bimalmaity8440@gmail.com>
2026-09-24 01:23:59 +0530

    next

--- .history/backend/routes/seatMapRoutes_20260919154903.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const seatMapController = require('../controllers/seatMapController');
-
-router.post('/save', seatMapController.saveSeatMap);
-router.get('/:id', seatMapController.getSeatMapById);
-
-module.exports = router;
